34165a87dcf6f6f55afe1cb0988de37e007a4751
DevTony322 <2016236988@unfv.edu.pe>
2019-04-02 06:29:25 -0500

    Arreglando y agregando datos

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello ZUM' });
+  res.render('index', { title: 'HelloZUM' });
 });
 /* GET response helloZUM. */
 router.get("/rest",function(req,res){
