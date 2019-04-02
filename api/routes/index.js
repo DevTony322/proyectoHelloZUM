ad641d3a04b8d66633cb32f2288224d487522fd4
DevTony322 <2016236988@unfv.edu.pe>
2019-04-01 22:19:46 -0500

    First Commit

---
+++ api/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello ZUM' });
+});
+/* GET response helloZUM. */
+router.get("/rest",function(req,res){
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ helloZum: 'Bienvenido a HelloZUM' }));
+});
+
+module.exports = router;
